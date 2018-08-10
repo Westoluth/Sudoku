20fc2806bb64a39e6f3011812a64c6b47f691412
Eric Vin <ericvin@outlook.com>
2018-08-09 20:26:22 -0700

    Set up framework for Board.java Unit tests
    -Set up frame for Board.java unit tests. No actual test code implemented yet.

---
+++ test/src/sudokutest/tests/BoardTest.java
@@ -0,0 +1,15 @@
+package sudokutest.unittests;
+
+import org.testng.annotations.*;
+
+import sudoku.Board;
+
+public class BoardTest {
+	/*
+	This test creates a board and verifies that all tiles are created and sorted into the correct spot
+	*/
+	@Test(groups = {"checkin", "full"})
+	public void creationTest() {
+
+	}
+}
