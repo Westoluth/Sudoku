56fcdcd19fa080dd9f33be2987a9e48f4771772e
Eric Vin <ericvin@outlook.com>
2018-08-20 11:21:18 -0700

    Changed access on classes in sudoku.board to better hide data
    -Changed many fields to protected
    -Added additional getter functions to help access data

--- src/sudoku/board/Region.java
+++ src/sudoku/board/Region.java
@@ -8,26 +8,22 @@ public class Region {
 	Region variables:
 		-regionCells: All cells contained in this region
 	*/
-	public Cell[] regionCells;
+	protected Cell[] regionCells;
 
-	public Region() {
+	protected Region() {
 		//Initializes regionCells to hold 9 cells
 		regionCells = new Cell[9];
 	}
 
-	/*
-	Adds new cell to Region
-	*/
-	public void addCell(Cell newCell) {
-		for(int cellNum = 0; cellNum < regionCells.length; cellNum++) {
-			if(regionCells[cellNum] == null) {
-				regionCells[cellNum] = newCell;
-				return;
-			}
-		}
+	/*--------------------------------------------------------------------------------
+	// Public Getter Methods 
+	--------------------------------------------------------------------------------*/
 
-		//Thrown if Region is full
-		throw new RuntimeException("RegionOverfilled: Attempted to add new cell to filled region");
+	/*
+	Returns the array regionCells
+	*/
+	public Cell[] getCells() {
+		return regionCells;
 	}
 
 	public String toString() {
@@ -43,5 +39,24 @@ public class Region {
 		output = output.substring(0, output.length()-2);
 
 		return output;
+	}
+
+	/*--------------------------------------------------------------------------------
+	// Protected Setup Functions
+	--------------------------------------------------------------------------------*/
+
+	/*
+	Adds new cell to Region
+	*/
+	protected void addCell(Cell newCell) {
+		for(int cellNum = 0; cellNum < regionCells.length; cellNum++) {
+			if(regionCells[cellNum] == null) {
+				regionCells[cellNum] = newCell;
+				return;
+			}
+		}
+
+		//Thrown if Region is full
+		throw new RuntimeException("RegionOverfilled: Attempted to add new cell to filled region");
 	}
 }
