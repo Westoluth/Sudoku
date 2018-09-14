14b2969edb2544e930d1c11daec6f2371974944a
Eric Vin <ericvin@outlook.com>
2018-09-14 10:28:05 -0700

    Completed first iteration of SetCellValue (Untested)
    -Fleshed out SetCellValue to be given 2 paramaters indicating cell
     location and value when created. Can then apply those to any SolverBoard
     by running apply action on the passed inputBoard

--- src/sudoku/solver/actions/SetCellValue.java
+++ src/sudoku/solver/actions/SetCellValue.java
@@ -2,8 +2,26 @@ package sudoku.solver.actions;
 
 import sudoku.solver.solvercontext.*;
 
+/*
+An Action to set a cell at cellId in inputBoard to cellValue.
+*/
 public class SetCellValue implements Action {
+	/*
+	SetCellValue variables:
+		-cellId: The location of the cell in inputBoard's boardCells array
+		-cellValue: The value that the selected cell should be set to
+	*/
+
+	int cellId;
+	int cellValue;
+
+	public SetCellValue(int cellId, int cellValue) {
+		this.cellId = cellId;
+		this.cellValue = cellValue;
+	}
+
 	public void applyAction(SolverBoard inputBoard) {
-		
+		SolverCell targetCell = inputBoard.getCell(cellId);
+		targetCell.setValue(cellValue);
 	}
 }
