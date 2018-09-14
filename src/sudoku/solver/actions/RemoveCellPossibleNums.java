2f78974a948a22dfb6e81ec2e157b144ab4dfea4
Eric Vin <ericvin@outlook.com>
2018-09-14 11:39:49 -0700

    Merged RemoveCellPossibleNum into RemoveCellPossibleNums and completed class
    -Merged RemoveCellPossibleNum inro RemoveCellPossibleNums, and added a
     single int constructor to RemoveCellPossibleNums. It can now take a
     single int or an array of ints.
    -Completed applyAction function to call removePossibleNum on the
     targett SolverCell for each number to be removed.

--- src/sudoku/solver/actions/RemoveCellPossibleNums.java
+++ src/sudoku/solver/actions/RemoveCellPossibleNums.java
@@ -3,24 +3,45 @@ package sudoku.solver.actions;
 import sudoku.solver.solvercontext.*;
 
 /*
-An Action to remove multiple possible numbers from a SolverCell
+An Action to remove one or more possible numbers from a SolverCell
 */
 public class RemoveCellPossibleNums implements Action {
 	/*
 	RemoveCellPossibleNum variables:
 		-cellId: The location of the cell in inputBoard's boardCells array
-		-possibleNums: The possible numbers to be removed from Cell
+		-removedNums: The possible numbers to be removed from Cell
 	*/
 
 	int cellId;
-	int[] possibleNums;
+	int[] removedNums;
 
-	public RemoveCellPossibleNums(int cellId, int[] possibleNums) {
+	/*
+	Constructor to remove just one possible number from a cell 
+	*/
+	public RemoveCellPossibleNums(int cellId, int removedNum) {
 		this.cellId = cellId;
-		this.possibleNums = possibleNums;
+		removedNums = new int[1];
+		removedNums[0] = removedNum;
 	}
 
-	public void applyAction(SolverBoard inputBoard) {
+	/*
+	Constructor to remove multiple possible numbers from a cell
+	*/
+	public RemoveCellPossibleNums(int cellId, int[] removedNums) {
+		this.cellId = cellId;
+		this.removedNums = removedNums;
+	}
 
+	/*
+	Removes removedNums from a cell specified by cellId's possibleNums
+	*/
+	public void applyAction(SolverBoard inputBoard) {
+		//Finds targetCell based of cellId
+		SolverCell targetCell =  inputBoard.getCell(cellId);
+
+		//Removes all numbers in removedNums from targetCell's possibleNums
+		for(int removedNumsIt = 0; removedNumsIt < removedNums.length; removedNumsIt++) {
+			targetCell.removePossibleNum(removedNums[removedNumsIt]);
+		}
 	}
 }
